adb9dd09e09e5e6423976af826dd79b9aa7b499f
Vitor Daniel <vitordaniel602@gmail.com>
2023-04-10 17:20:17 -0300

    Install InertialJs and initialize the authentication module

---
+++ resources/js/Pages/dashboard.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import { Head } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+    <p>Hello Vitor, welcome to your first Inertia app!</p>
+</template>
