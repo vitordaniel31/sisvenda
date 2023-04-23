0f92662888436b8c064c6b24599944d2209544b6
ketwy <ketllyazevedo14@gmail.com>
2023-04-23 11:51:01 -0300

    fix: alterando ordem do permissionSeeder #21

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            RoleSeeder::class,
             PermissionSeeder::class,
+            RoleSeeder::class,
             UserSeeder::class,
         ]);
     }
