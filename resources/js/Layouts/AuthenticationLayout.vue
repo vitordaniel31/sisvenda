16818b1643fff7dc6ef4b9c3a28bc38864e80ebe
vitor_medeiros <vitordaniel602@gmail.com>
2023-05-02 15:58:57 -0300

    Alterando cor do template

--- resources/js/Layouts/AuthenticationLayout.vue
+++ resources/js/Layouts/AuthenticationLayout.vue
@@ -12,7 +12,7 @@ export default {
     <MasterLayout title="Autenticação">
         <template #layout>
             <div style="height: 100vh" class="d-flex align-items-center">
-                <div class="row justify-content-center" style="width: 200vh">
+                <div class="row justify-content-center" style="width: 250vh">
                     <div class="col-lg-5 col-md-10">
                         <div class="card o-hidden border-0 shadow-lg mx-2">
                             <div class="card-body p-0">
