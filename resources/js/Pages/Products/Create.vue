fecb995d41e1d8419913be1006ea450ecaa32ca9
ketwy <ketllyazevedo14@gmail.com>
2023-05-08 09:56:15 -0300

    feat: criando template de Criar Produto #28

---
+++ resources/js/Pages/Products/Create.vue
@@ -0,0 +1,57 @@
+<script>
+import DashboardLayout from "@/Layouts/DashboardLayout.vue";
+import { Link, useForm } from "@inertiajs/vue3";
+import Form from "./Form.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+
+export default {
+    components: {
+        DashboardLayout,
+        Link,
+        Form,
+        PrimaryButton,
+    },
+
+    data() {
+        return {
+            title: "Produtos",
+            description: "Cadastrar Produto",
+            form: useForm({
+                name: "",
+                price: "",
+            }),
+        };
+    },
+
+    methods: {
+        submit() {
+            this.form.post(route("products.store"));
+        },
+    },
+};
+</script>
+
+<template>
+    <DashboardLayout :title="title" :description="description">
+        <template #breadcrumbs>
+            <li class="breadcrumb-item">
+                <Link :href="route('products.index')">Produtos</Link>
+            </li>
+            <li class="breadcrumb-item active">Cadastrar Produto</li>
+        </template>
+        <template #content>
+            <form @submit.prevent="submit">
+                <div class="col-lg-12">
+                    <Form :form="form" :create="true" :disabled="false"></Form>
+                </div>
+                <div class="col-lg-12">
+                    <div class="row justify-content-center">
+                        <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            Confirmar
+                        </PrimaryButton>
+                    </div>
+                </div>
+            </form>
+        </template>
+    </DashboardLayout>
+</template>
