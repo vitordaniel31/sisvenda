862384aa4be5fd08f784b5de9b4aa65c310378dc
ketwy <ketllyazevedo14@gmail.com>
2023-05-08 09:51:14 -0300

    fix: organizando migrate de produto #28

--- database/migrations/2023_05_07_171329_create_products_table.php
+++ database/migrations/2023_05_07_171329_create_products_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price', 8, 2);
             $table->timestamps();
+            $table->softDeletes();
+
         });
     }
 
