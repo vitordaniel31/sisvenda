19b768efd1b3a0b011cba8577eefd659ca6b7600
ketwy <ketllyazevedo14@gmail.com>
2023-05-07 22:12:18 -0300

    fiz: criação do models de produto #28

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory,
+        SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'price',
+    ];
 }
