3c147e3d84295b1ea8b3c56f9cd4d7bed0a56ba5
ketwy <ketllyazevedo14@gmail.com>
2023-05-08 09:52:13 -0300

    fix: adicionando importação de User #28

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -11,7 +11,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,8 +22,8 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['string', 'max:255'],
-            'price' => ['price', 'numeric', 'regex:/^\d+(\.\d{1,3})?$/']
+            'name' => ['required','string', 'max:255'],
+            'price' => ['required', 'numeric', 'regex:/^\d+(\.\d{1,3})?$/']
         ];
     }
 }
