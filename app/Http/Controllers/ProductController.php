399a34291df20394224d6691ab9d5d3d3823ec61
ketwy <ketllyazevedo14@gmail.com>
2023-05-07 22:08:57 -0300

    feat: criação de crontroller de produtos #28

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    public function __construct()
+    {
+        $this->authorizeResource(Product::class);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        $products = collect($products)->map(function ($product) {
+            $product['roles'] = implode(', ', $product->getRoleNames()->toArray());
+            $product['can_update'] = auth()->user()->can('update', $product);
+            $product['can_delete'] = auth()->user()->can('delete', $product);
+            return $product;
+        }, $products);
+
+        return Inertia::render('Products/Index', [
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Products/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric|regex:/^\d+(\.\d{1,3})?$/',
+        ]);
+
+        $product = Product::create();
+
+        session()->flash('alert', [
+            'type' => 'success',
+            'message' => 'O produto foi criado com sucesso.'
+        ]);
+
+        return Redirect::route('products.show', $product);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        $product['can_update'] = auth()->user()->can('update', $product);
+        $product['can_delete'] = auth()->user()->can('delete', $product);
+
+        return Inertia::render('Products/Show', [
+            'product' => $product
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        return Inertia::render('Products/Edit', [
+            'product' => $product
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric|regex:/^\d+(\.\d{1,3})?$/',
+        ]);
+
+        $product->update();
+
+
+        session()->flash('alert', [
+            'type' => 'success',
+            'message' => 'O produto foi atualizado com sucesso.'
+        ]);
+
+        return Redirect::route('products.show', $product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        session()->flash('alert', [
+            'type' => 'success',
+            'message' => 'O produto foi deletado com sucesso.',
+        ]);
+
+        return Redirect::route('products.index');
+    }
+}
