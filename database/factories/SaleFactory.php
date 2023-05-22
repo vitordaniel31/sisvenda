82932c932d451a694a378949699e894d3e0b3e44
Hugo Maia <maiahugom123@gmail.com>
2023-05-22 17:39:56 -0300

    atualizando database/factories/SaleFactory issue #62

---
+++ database/factories/SaleFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class SaleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => fake()->unique()->word(),
+            'date' => fake()->dateTimeBetween('1 year, now'),
+        ];
+    }
+}
