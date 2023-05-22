303e42ed505f57eb3272f3295be5087dc6a72f64
Hugo Maia <maiahugom123@gmail.com>
2023-05-22 17:48:45 -0300

    atualizando database/migrations/create_sales_table issue #62

--- database/migrations/2023_05_22_140916_create_sales_table.php
+++ database/migrations/2023_05_22_140916_create_sales_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->softDeletes();
+            $table->date('date');
+            $table->string('user_id');
+
         });
     }
 
