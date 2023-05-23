9b0ff17c896818cdc228d09adcc93bc9b92d664c
José Marques <jose.marques15@hotmail.com>
2023-05-22 21:24:28 -0300

    test: testando o cadastrar produtos #51

---
+++ tests/Unit/CreateProductTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use App\Models\Product;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+uses(Tests\TestCase::class);
+
+it('create a product with permission', function () {
+    $user = User::factory()->create();
+    $product = Product::factory()->create();
+    $role = Role::firstOrCreate([
+        'name' => 'Administrador',
+        'guard_name' => '*'
+    ]);
+
+    $permission = Permission::firstOrCreate([
+        'name' => 'products.create',
+        'guard_name' => '*'
+    ]);
+
+    $user->syncPermissions($permission->id);
+    $user->syncRoles($role->id);
+
+    $this->actingAs($user)
+    ->post(route('products.store', $product->toArray()))
+    ->assertRedirect(route('products.show', Product::latest()->first()));
+
+});
+
+it('create a product without permission', function () {
+    $user = User::factory()->create();
+    $product = Product::factory()->create();
+
+    $this->actingAs($user)->post(route('products.store', $product))
+        ->assertStatus(403);
+});
