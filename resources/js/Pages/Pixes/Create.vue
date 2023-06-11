9c053fa514e81e317b1f449f9157bf297c45d830
José Marques <jose.marques15@hotmail.com>
2023-06-11 19:57:38 -0300

    feat: criação e configuração das páginas Pix #74

---
+++ resources/js/Pages/Pixes/Create.vue
@@ -0,0 +1,61 @@
+<script>
+import DashboardLayout from "@/Layouts/DashboardLayout.vue";
+import { Link, useForm } from "@inertiajs/vue3";
+import Form from "./Form.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+
+export default {
+    components: {
+        DashboardLayout,
+        Link,
+        Form,
+        PrimaryButton,
+    },
+
+    data() {
+        return {
+            title: "Pix",
+            description: "Cadastrar Pix",
+            form: useForm({
+                name: "",
+                key: "",
+                type: "",
+            }),
+        };
+    },
+
+    methods: {
+        submit() {
+            this.form.post(route("pixes.store"));
+        },
+    },
+};
+</script>
+
+<template>
+    <DashboardLayout :title="title" :description="description">
+        <template #breadcrumbs>
+            <li class="breadcrumb-item">
+                <Link :href="route('pixes.index')">Pix</Link>
+            </li>
+            <li class="breadcrumb-item active">Cadastrar Pix</li>
+        </template>
+        <template #content>
+            <form @submit.prevent="submit">
+                <div class="col-lg-12">
+                    <Form :form="form" :create="true" :disabled="false"></Form>
+                </div>
+                <div class="col-lg-12">
+                    <div class="row justify-content-center">
+                        <PrimaryButton
+                            :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing"
+                        >
+                            Confirmar
+                        </PrimaryButton>
+                    </div>
+                </div>
+            </form>
+        </template>
+    </DashboardLayout>
+</template>
---
+++ resources/js/Pages/Pixes/Form.vue
@@ -0,0 +1,85 @@
+<script>
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { Link } from "@inertiajs/vue3";
+
+export default {
+    components: {
+        TextInput,
+        InputLabel,
+        InputError,
+    },
+
+    props: {
+        create: {
+            type: Boolean,
+        },
+
+        disabled: {
+            type: Boolean,
+        },
+
+        form: {
+            type: Object,
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="row justify-content-center">
+        <div class="col-lg-5">
+            <div class="form-group">
+                <InputLabel for="name" value="Nome" :required="true" />
+
+                <TextInput
+                    id="name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.name"
+                    required
+                    autofocus
+                    autocomplete="name"
+                    :disabled="disabled"
+                />
+
+                <InputError class="mt-2" :message="form.errors.name" />
+            </div>
+        </div>
+        <div class="col-lg-5">
+            <div class="form-group">
+                <InputLabel for="key" value="Chave" :required="true" />
+                <TextInput
+                    id="key"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.key"
+                    required
+                    autofocus
+                    autocomplete="key"
+                    :disabled="disabled"
+                />
+
+                <InputError class="mt-2" :message="form.errors.key" />
+            </div>
+        </div>
+        <div class="col-lg-5">
+            <div class="form-group">
+                <InputLabel for="type" value="Tipo" :required="true" />
+                <TextInput
+                    id="type"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.type"
+                    required
+                    autofocus
+                    autocomplete="type"
+                    :disabled="disabled"
+                />
+
+                <InputError class="mt-2" :message="form.errors.type" />
+            </div>
+        </div>
+    </div>
+</template>
