07f83dcff111b32a99b0b7a1c9bdac877b61b5bc
José Marques <jose.marques15@hotmail.com>
2023-06-12 17:04:09 -0300

    fix: correções para o type_id #74

--- app/Http/Requests/UpdatePixRequest.php
+++ app/Http/Requests/UpdatePixRequest.php
@@ -24,7 +24,7 @@ class UpdatePixRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'key' => ['required', 'string', 'max:255'],
-            'type' => ['required', 'string', 'max:255']
+            'type_id' => ['required', 'in:0,1,2,3']
         ];
     }
 }
