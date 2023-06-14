7e0781b3accc752582ada44c38668d600e4a8eeb
ketwy <ketllyazevedo14@gmail.com>
2023-06-14 00:12:07 -0300

    fix: site finalizado

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -15,7 +15,7 @@
     <div class="body-wrap boxed-container">
         <header class="site-header">
             <div class="container">
-                <div class="site-header-inner">
+                <div class="site-header-inner" style="display: flex; justify-content: space-between;">
                     <div class="brand header-brand">
                         <h1 class="m-0">
                             <a href="#">
@@ -23,6 +23,7 @@
                             </a>
                         </h1>
                     </div>
+                    <a class="button button-sm button-primary button-shadow" href="#">login</a>
                 </div>
             </div>
         </header>
@@ -114,10 +115,8 @@
                         </div>
                         <div class="features-wrap">
                             <div class="feature text-center is-revealing">
-                                <div class="feature-inner">
-                                    <div class="feature-icon">
-                                        <image src="{{ asset('template-landing-page/src/images/panda.png') }}"/> 
-                                    </div>
+                                <div class="feature-inner" style="text-align: center;">
+                                    <img src="{{ asset('template-landing-page/src/images/panda.png') }}" style="display: block; margin: 0 auto;"/>
                                     <h4 class="feature-title h3-mobile mb-8">Bottons 37mm</h4>
                                     <table class="table table-bordered">
                                         <thead>
@@ -152,19 +151,8 @@
                                 </div>
                             </div>
                             <div class="feature text-center is-revealing">
-                                <div class="feature-inner">
-                                    <div class="feature-icon" style="background:#FFD8CD;">
-                                        <svg width="88" height="88" xmlns="http://www.w3.org/2000/svg">
-                                            <g fill="none" fill-rule="nonzero">
-                                                <path
-                                                    d="M54 56h-9a2 2 0 0 1-2-2V43a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2zm-9-13v10h9V43h-9z"
-                                                    fill="#FCAC96" />
-                                                <path
-                                                    d="M41 50h-7V34h14v5h2v-5a2 2 0 0 0-2-2H34a2 2 0 0 0-2 2v18a2 2 0 0 0 2 2h7v-4z"
-                                                    fill="#FC8464" />
-                                            </g>
-                                        </svg>
-                                    </div>
+                                <div class="feature-inner" style="text-align: center;">
+                                    <image src="{{ asset('template-landing-page/src/images/porco.png') }}" style="display: block; margin: 0 auto;"/> 
                                     <h4 class="feature-title h3-mobile mb-8">Bottons 25mm</h4>
                                     <table class="table table-bordered">
                                         <thead>
@@ -191,11 +179,9 @@
                                 </div>
                             </div>
                             <div class="feature text-center is-revealing">
-                                <div class="feature-inner">
-                                    <div class="feature-icon" style="background:#C6FDF3;">
-                                        <img class="mb-16" src="{{ asset('template-landing-page/dist/images/.png') }}" alt="Testimonial">
-                                    </div>
-                                    <h4 class="feature-title h3-mobile mb-8">bottons Holográficos</h4>
+                                <div class="feature-inner" style="text-align: center;">
+                                    <img src="{{ asset('template-landing-page/src/images/borboleta.png') }}" style="display: block; margin: 0 auto;"/>
+                                    <h4 class="feature-title h3-mobile mb-8">Bottons Holográficos</h4>
                                     <table class="table table-bordered">
                                         <thead>
                                             <tr>
@@ -222,8 +208,8 @@
                     <svg width="280" height="280" viewBox="0 0 280 280" xmlns="http://www.w3.org/2000/svg">
                         <defs>
                             <linearGradient x1="100%" y1="0%" x2="0%" y2="100%" id="testimonials-shape-1">
-                                <stop stop-color="#261FB6" offset="0%" />
-                                <stop stop-color="#4950F6" offset="100%" />
+                                <stop stop-color="#31AFF8" offset="0%" />
+                                <stop stop-color="#38B6FF" offset="100%" />
                             </linearGradient>
                         </defs>
                         <circle cx="140" cy="685" r="140" transform="translate(0 -545)"
@@ -258,7 +244,7 @@
                 </div>
                 <div class="container">
                     <div class="testimonials-inner section-inner">
-                        <h2 class="section-title mt-0 text-center">Testimonials</h2>
+                        <h2 class="section-title mt-0 text-center">Feedbacks</h2>
                         <div class="testimonials-wrap">
                             <div class="testimonial text-xs is-revealing">
                                 <div class="testimonial-inner">
@@ -269,13 +255,12 @@
                                                 alt="Testimonial">
                                         </div>
                                         <div class="testimonial-body">
-                                            <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                                sed do eiusmod tempor incididunt.</p>
+                                            <p class="mb-0">Adorei a qualidade dos bottons que comprei nesta loja! Eles são duráveis e as cores são vibrantes. Com certeza voltarei a comprar mais para adicionar ao meu estilo único.</p>
                                         </div>
                                     </div>
                                     <div class="testimonial-footer">
                                         <div class="testimonial-link">
-                                            <a href="#">@martajones</a>
+                                            <a href="#">Eduarda</a>
                                         </div>
                                     </div>
                                 </div>
@@ -289,13 +274,12 @@
                                                 alt="Testimonial">
                                         </div>
                                         <div class="testimonial-body">
-                                            <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                                sed do eiusmod tempor incididunt.</p>
+                                            <p class="mb-0">Fiquei impressionado com a variedade de opções disponíveis nesta loja de bottons. Desde os designs mais clássicos até os mais modernos, encontrei exatamente o que estava procurando. A entrega foi rápida e o atendimento ao cliente foi excelente. Recomendo!</p>
                                         </div>
                                     </div>
                                     <div class="testimonial-footer">
                                         <div class="testimonial-link">
-                                            <a href="#">@michealpahm</a>
+                                            <a href="#">Lucas</a>
                                         </div>
                                     </div>
                                 </div>
@@ -309,13 +293,12 @@
                                                 alt="Testimonial">
                                         </div>
                                         <div class="testimonial-body">
-                                            <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                                sed do eiusmod tempor incididunt.</p>
-                                        </div>
+                                            <p class="mb-0">Essa loja de bottons superou todas as minhas expectativas! Os produtos são de alta qualidade e o preço é muito justo. Além disso, o envio foi muito rápido e a embalagem estava perfeita, garantindo que os bottons chegassem em perfeito estado. Com certeza indicarei essa loja para meus amigos e familiares.</p>
+                                        q</div>
                                     </div>
                                     <div class="testimonial-footer">
                                         <div class="testimonial-link">
-                                            <a href="#">@markbrown</a>
+                                            <a href="#"> Gustavo </a>
                                         </div>
                                     </div>
                                 </div>
@@ -329,19 +312,9 @@
                 <div class="container-sm">
                     <div class="newsletter-inner section-inner">
                         <div class="newsletter-header text-center">
-                            <h2 class="section-title mt-0">Stay in the know</h2>
-                            <p class="section-paragraph">Lorem ipsum is common placeholder text used to demonstrate
-                                the
-                                graphic elements of a document or visual presentation.</p>
-                        </div>
-                        <div class="footer-form newsletter-form field field-grouped">
-                            <div class="control control-expanded">
-                                <input class="input" type="email" name="email" placeholder="Your best email&hellip;">
-                            </div>
-                            <div class="control">
-                                <a class="button button-primary button-block button-shadow" href="#">Early
-                                    access</a>
-                            </div>
+                            <h2 class="section-title mt-0">Entre em Contato</h2>
+                            <p class="section-paragraph">Estamos aqui para atender às suas necessidades personalizadas! Entre em contato conosco agora mesmo e faça o seu pedido. Estamos ansiosos para criar algo especial para você!</p>
+                            <a class="button button-lg button-shadow" href="https://wa.me/c/558499292655">Chame no Whatsapp</a>
                         </div>
                     </div>
                 </div>
@@ -353,91 +326,25 @@
                 <div class="site-footer-inner has-top-divider">
                     <div class="brand footer-brand">
                         <a href="#">
-                            <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
-                                <title>April</title>
-                                <defs>
-                                    <linearGradient x1="114.674%" y1="39.507%" x2="-52.998%" y2="39.507%"
-                                        id="logo-footer-a">
-                                        <stop stop-color="#8D92FA" offset="0%" />
-                                        <stop stop-color="#8D92FA" stop-opacity="0" offset="100%" />
-                                    </linearGradient>
-                                    <linearGradient x1="93.05%" y1="19.767%" x2="15.034%" y2="85.765%"
-                                        id="logo-footer-b">
-                                        <stop stop-color="#FF3058" offset="0%" />
-                                        <stop stop-color="#FF6381" offset="100%" />
-                                    </linearGradient>
-                                    <linearGradient x1="32.716%" y1="-20.176%" x2="32.716%" y2="148.747%"
-                                        id="logo-footer-c">
-                                        <stop stop-color="#FF97AA" offset="0%" />
-                                        <stop stop-color="#FF97AA" stop-opacity="0" offset="100%" />
-                                    </linearGradient>
-                                </defs>
-                                <g fill="none" fill-rule="evenodd">
-                                    <path
-                                        d="M31.12 7.482C28.327 19.146 19.147 28.326 7.483 31.121A12.04 12.04 0 0 1 .88 24.518C3.674 12.854 12.854 3.674 24.518.879a12.04 12.04 0 0 1 6.603 6.603z"
-                                        fill="#312ECA" />
-                                    <path
-                                        d="M28.874 3.922l-24.91 24.99a12.026 12.026 0 0 1-3.085-4.394C3.674 12.854 12.854 3.674 24.518.879a12.025 12.025 0 0 1 4.356 3.043z"
-                                        fill="url(#logo-footer-a)" />
-                                    <g opacity=".88">
-                                        <path
-                                            d="M31.12 24.518a12.04 12.04 0 0 1-6.602 6.603C12.854 28.326 3.674 19.146.879 7.482A12.04 12.04 0 0 1 7.482.88c11.664 2.795 20.844 11.975 23.639 23.639z"
-                                            fill="url(#logo-footer-b)" />
-                                        <path
-                                            d="M24.518 31.12C12.854 28.327 3.674 19.147.879 7.483A12.015 12.015 0 0 1 3.46 3.57L28.47 28.5a12.016 12.016 0 0 1-3.951 2.62z"
-                                            fill="url(#logo-footer-c)" />
-                                    </g>
-                                </g>
-                            </svg>
+                            <img src="{{ asset('template-landing-page/src/images/icon.png') }}" width="60px"/> 
                         </a>
                     </div>
-                    <ul class="footer-links list-reset">
-                        <li>
-                            <a href="#">Contact</a>
-                        </li>
-                        <li>
-                            <a href="#">About us</a>
-                        </li>
-                        <li>
-                            <a href="#">FAQ's</a>
-                        </li>
-                        <li>
-                            <a href="#">Support</a>
-                        </li>
-                    </ul>
                     <ul class="footer-social-links list-reset">
                         <li>
-                            <a href="#">
-                                <span class="screen-reader-text">Facebook</span>
-                                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M6.023 16L6 9H3V6h3V4c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V6H13l-1 3H9.28v7H6.023z"
-                                        fill="#FFF" />
-                                </svg>
+                            <a href="https://www.instagram.com/menina_dos_bottons_/" target="_blank">
+                                <span class="screen-reader-text">Instagram</span>
+                                <img src="{{ asset('template-landing-page/src/images/instagram.svg') }}" width="16" height="16"/> 
+                                
                             </a>
                         </li>
                         <li>
-                            <a href="#">
-                                <span class="screen-reader-text">Twitter</span>
-                                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M16 3c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4C.7 7.7 1.8 9 3.3 9.3c-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H0c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4C15 4.3 15.6 3.7 16 3z"
-                                        fill="#FFF" />
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="screen-reader-text">Google</span>
-                                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M7.9 7v2.4H12c-.2 1-1.2 3-4 3-2.4 0-4.3-2-4.3-4.4 0-2.4 2-4.4 4.3-4.4 1.4 0 2.3.6 2.8 1.1l1.9-1.8C11.5 1.7 9.9 1 8 1 4.1 1 1 4.1 1 8s3.1 7 7 7c4 0 6.7-2.8 6.7-6.8 0-.5 0-.8-.1-1.2H7.9z"
-                                        fill="#FFF" />
-                                </svg>
+                            <a href="https://wa.me/c/558499292655">
+                                <span class="screen-reader-text">Whatsapp</span>
+                                <img src="{{ asset('template-landing-page/src/images/zap.svg') }}" width="16" height="16"/> 
                             </a>
                         </li>
                     </ul>
-                    <div class="footer-copyright">&copy; 2018 April, all rights reserved</div>
+                    <div class="footer-copyright">Menina Dos Bottons &copy; 2023 de junho</div>
                 </div>
             </div>
         </footer>
