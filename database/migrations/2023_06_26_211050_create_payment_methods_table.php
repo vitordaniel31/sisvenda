195f3613c1b3396e7e861697ccbd85bae61fc4eb
José Marques <jose.marques15@hotmail.com>
2023-06-27 15:39:42 -0300

    feat: Manter Forma de Pagamento #91

---
+++ database/migrations/2023_06_26_211050_create_payment_methods_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('name_id');
+            $table->unsignedBigInteger('pix_id')->nullable();
+            $table->foreign('pix_id')->references('id')->on('pixes');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_methods');
+    } 
+};
