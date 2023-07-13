000cf4be1805026dad6cec38c481d693494d035a
ketwy <ketllyazevedo14@gmail.com>
2023-07-12 22:34:47 -0300

    fix: configurando povoamento #102

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
+            ProductSeeder::class,
+            PixSeeder::class
         ]);
     }
 }
